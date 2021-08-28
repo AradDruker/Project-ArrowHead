056c66ceed1632487ec0fa83c3034722ef6ab776
Nitz-alt <nitzanyizhar@gmail.com>
2021-08-28 23:34:15 +0300

    Enemy movement
    Enemy currently chasing mouse
    *Need to change it to chase player when combining everything

---
+++ Scenes/Enemy/KinematicBody2D.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+var velocity = Vector2(1.0, 1.0)
+const MAX_SPEED = 400
+
+
+func _physics_process(delta):
+	# Mouse position for enemy to focus on
+	var mouse_pos = get_viewport().get_mouse_position()
+	# Vector towards the mouse
+	var v = Vector2(position.x, position.y)
+	velocity = v.direction_to(Vector2(mouse_pos))
+	move_and_collide(velocity* delta * MAX_SPEED)
