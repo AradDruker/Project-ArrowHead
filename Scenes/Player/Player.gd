f96d9f8bedf8b93c78261b9d08240508632d98c5
Nitz-alt <nitzanyizhar@gmail.com>
2021-08-28 17:37:12 +0300

    Player Movement and Borders
    Now bounces from borders

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,14 +1,41 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
+const MAX_SPEED = 10
+const ACCELERATION = 30
+const FRICTION = 5
 
+
+func _ready():
+	position = Vector2(640.0, 360.0)
 
 func _physics_process(delta):
-	var inp = Vector2.ZERO
-	inp.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	inp.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	if inp != Vector2.ZERO:
-		velocity += inp
+	var screen_size = get_viewport_rect().size
+	# Input Collection
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED)
+		var ang = atan2(velocity.x, velocity.y)
+		print(rad2deg(ang))
+		$ShipSprite.rotation = PI - ang
+		
+	
 	else:
-		velocity = Vector2.ZERO
+		velocity -= velocity * FRICTION * delta
+	
+	var posx = position.x
+	var posy = position.y
+	if posy <= 10 or posy >= screen_size.y - 10 or posx <= 10 or posx >= screen_size.x - 10:
+		velocity = velocity.rotated(PI/2)
+		
 	move_and_collide(velocity)
+	
+	# Keeps player in window
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
