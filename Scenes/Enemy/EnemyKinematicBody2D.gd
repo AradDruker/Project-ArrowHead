39c623bbe72aabaeccd37764a63679ba50a83214
Nitz-alt <nitzanyizhar@gmail.com>
2021-08-31 15:55:12 +0300

    Changed Enemy Movement
    Added randomization and removes when exited the game/screen.

--- Scenes/Enemy/EnemyKinematicBody2D.gd
+++ Scenes/Enemy/EnemyKinematicBody2D.gd
@@ -1,10 +1,22 @@
 extends KinematicBody2D
 
+
 var velocity = Vector2(1.0, 1.0)
-const MAX_SPEED = 100
-const ACCELERATION =  10
+var MAX_SPEED = 300
+var ACCELERATION
 var player_pos
 
+export (NodePath) var visibility_notifier_path
+
+
+
+
+func _ready():
+	randomize()
+	ACCELERATION = randi() % 10 + 5
+	MAX_SPEED = randi() % 201 + 200
+	var visibility_notifier = get_node(visibility_notifier_path)
+	
 
 func player_details(pos):
 	player_pos = pos
@@ -16,15 +28,24 @@ func _physics_process(delta):
 	# Mouse position for enemy to focus on
 	
 	# Vector towards the mouse
+	#player_pos = get_viewport().get_mouse_position()
 	var v = Vector2(position.x, position.y)
 	v = v.direction_to(Vector2(player_pos))
-	velocity +=  v.normalized() * ACCELERATION
+	velocity +=  v * ACCELERATION
+	velocity = velocity.clamped(MAX_SPEED)
+	
+	
+	
 	
 	# Making ship point to the destination
 	var ang = atan2(velocity.x, velocity.y)
 	$EnemySprite.rotation = PI - ang
 	
 	if pow(pow(position.x - player_pos.x, 2) + pow(position.y - player_pos.y, 2), 0.5) > 3:
-	
-		move_and_collide(velocity * delta * MAX_SPEED)
+		move_and_slide(velocity)
 
+
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	hide()
