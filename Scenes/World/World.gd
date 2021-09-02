e523dedc08316ea30cdfa281796d7007cb3375a9
Nitz-alt <nitzanyizhar@gmail.com>
2021-09-02 19:21:41 +0300

    Enemy spawn position
    Fixed problem where enemies sometimes spawned on or out of the border and not inside it

--- Scenes/World/World.gd
+++ Scenes/World/World.gd
@@ -5,6 +5,19 @@ export (PackedScene) var Enemy
 func _ready():
 	randomize()
 
+func calc_position(ratio):
+	# Get the number of slice to slice the screen
+	# Exmaple: Input 3 returns a random place within the 1/3 to 2/3 of the border
+	# Generates a random position within 1/3 to 2/3 of the screen
+	randomize()
+	var size = get_viewport().size
+	var x_pos = randi() % int(size.x)
+	var y_pos = randi() % int(size.y)
+	x_pos = clamp(x_pos, 100, size.x - 100)
+	y_pos = clamp(y_pos, 100, size.y - 100)
+	
+	return [x_pos, y_pos]
+
 func _physics_process(_delta):
 	var enemies = $Enemies.get_children()
 	for en in enemies:
@@ -19,29 +32,19 @@ func _on_EnemySpawnInstant_timeout():
 	var my_random_number = rng.randf_range(4,5)
 	for _i in my_random_number:
 		var enemy = Enemy.instance()
-		var size = get_viewport().size
-		var x_pos = randi() % int(size.x) + 30
-		var y_pos = randi() % int(size.y) + 30
-		x_pos = clamp(x_pos, 30, size.x)
-		y_pos = clamp(y_pos, 30, size.y)
-		enemy.position.x = x_pos
-		enemy.position.y = y_pos
+		var enemy_pos = calc_position(0.5)
+		enemy.position.x = enemy_pos[0]
+		enemy.position.y = enemy_pos[1]
 		$Enemies.add_child(enemy)
 		
 		
 func _on_EnemySpawn_timeout():
 	for _i in range(randi() % 5 + 2):
 		var enemy = Enemy.instance()
-		var size = get_viewport().size
-		var x_pos = randi() % int(size.x) + 30
-		var y_pos = randi() % int(size.y) + 30
-		x_pos = clamp(x_pos, 30, size.x)
-		y_pos = clamp(y_pos, 30, size.y)
-		enemy.position.x = x_pos
-		enemy.position.y = y_pos
+		var enemy_position = calc_position(0.5)
+		enemy.position.x = enemy_position[0]
+		enemy.position.y = enemy_position[1]
 		$Enemies.add_child(enemy)
-	
-	
 	
 	
 	
