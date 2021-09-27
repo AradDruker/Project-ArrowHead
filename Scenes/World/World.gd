d76cc030b35bb313b8b632b37a3ecb30013c5abe
Nitz-alt <nitzanyizhar@gmail.com>
2021-09-28 00:00:57 +0300

    Shield feature
    Shield feature done
    Only need to figure out how the player can attain it.

--- Scenes/World/World.gd
+++ Scenes/World/World.gd
@@ -32,8 +32,6 @@ func _ready():
 	#Sets mouse position to the center of the screen at game start
 	var screen_size = get_viewport().size
 	var start_pos = Vector2(640.0, 360.0)
-	var shield = create_shield()
-	$Shields.add_child(shield)
 	
 	### Signals for buttons
 # warning-ignore:return_value_discarded
